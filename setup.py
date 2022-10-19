b6f982be8464fd51031a5f86314f067ae1413514
TalB <talb@playtika.com>
2022-10-19 20:41:26 +0300

    AMASS segmentation & preprocessing added to git

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MeshSegmentation',
+    version='1.0',
+    packages=['mesh_segmentation', 'mesh_segmentation.data', 'mesh_segmentation.model', 'mesh_segmentation.utils', 'mesh_segmentation.training'],
+    url='https://github.com/TalBarami/MeshSegmentation',
+    license='',
+    author='TalB',
+    author_email='talbaramii@gmail.com',
+    description=''
+)
