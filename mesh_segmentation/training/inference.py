95b6c6f00d5c3238a51642d1f0227f8c25f64c80
TalB <talb@playtika.com>
2022-12-12 16:54:47 +0200

    Supports resuming.

--- mesh_segmentation/training/inference.py
+++ mesh_segmentation/training/inference.py
@@ -16,7 +16,8 @@ from mesh_segmentation.utils.segmentation_utils import get_best_model
 
 
 class Segmentor:
-    def __init__(self):
+    def __init__(self, checkpoints_dir):
+        self.checkpoints_dir = checkpoints_dir
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model_params = dict(
             in_features=3,
@@ -28,7 +29,7 @@ class Segmentor:
             num_heads=14,
             apply_batch_norm=True,
         )
-        self.model, self.epoch_id = get_best_model(self.model_params, self.device)
+        self.model, self.epoch_id = get_best_model(self.checkpoints_dir, self.model_params, self.device)
         self.pre_transform = Compose([FaceToEdge(remove_faces=False), NormalizeUnitSphere()])
 
     def predict(self, mesh):
@@ -39,18 +40,3 @@ class Segmentor:
         predictions = self.model(self.pre_transform(data))
         predicted_seg_labels = predictions.argmax(dim=-1, keepdim=True)
         return np.squeeze(predicted_seg_labels.detach().cpu().numpy())
-
-if __name__ == '__main__':
-    data_path = r'/mnt/DS_SHARED/users/talb/data/AMASS/h5'
-    data = 'test'
-    s = Segmentor()
-
-    with h5py.File(osp.join(data_path, 'segmentation', f'{data}.h5'), "r") as f:
-        pcs = np.array(f['point_cloud'])
-        faces = np.array(f['faces'])
-        skeletons = np.array(f['skeleton'])
-        segments = np.array(f['segmentations'])
-
-    for i, (pc, face, skeleton, org_segments) in tqdm(enumerate(zip(pcs, faces, skeletons, segments))):
-        mesh = trimesh.Trimesh(vertices=pc, faces=faces)
-        predicted_segments = s.predict(mesh)
